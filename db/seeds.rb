a673443aedd3197b01442d17f20bfc6d8f32b7fe
hoanghai096 <hai.hp.96@gmail.com>
2017-06-24 10:54:16 +0700

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ User.create full_name: "Hoàng Hải - Framgia",
   email: "hai.hp.96@gmail.com",
   password: "123456"
 
-10.times do |index|
+100.times do |index|
   User.create full_name: FFaker::NameVN.name,
     email: "example-#{index+1}@railstutorial.org",
     gender: Random.rand(0..2),
     password: "123456"
 end
 
-100.times do |index|
+1000.times do |index|
   Post.create title: FFaker::Education.degree_short,
     context: FFaker::Education.degree,
     user_id: Random.rand(1..10)
 end
 
-1000.times do |index|
+10000.times do |index|
   Comment.create context: FFaker::Lorem.phrase,
     post_id: Random.rand(1..100),
     user_id: Random.rand(1..10)
